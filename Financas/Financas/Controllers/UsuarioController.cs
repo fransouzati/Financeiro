b4744e71e54ba91a73868b127996c9bad77b7dca
EduardoSerqueira <eduardo.serqueira@outlook.com>
2016-07-31 13:59:43 -0300

    Finalização do cadastro de usuario

--- Financas/Financas/Controllers/UsuarioController.cs
+++ Financas/Financas/Controllers/UsuarioController.cs
@@ -1,4 +1,5 @@
-﻿using Financas.Entidades;
+﻿using Financas.DAO;
+using Financas.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,13 @@ namespace Financas.Controllers
 {
     public class UsuarioController : Controller
     {
-        // GET: Usuario
+        private UsuarioDAO usuarioDAO;
+
+        public UsuarioController(UsuarioDAO usuarioDAO)
+        {
+            this.usuarioDAO = usuarioDAO;
+        }
+
         public ActionResult Form()
         {
             return View();
@@ -19,12 +26,19 @@ namespace Financas.Controllers
         {
             if(ModelState.IsValid)
             {
-
+                usuarioDAO.Adiciona(usuario);
+                return RedirectToAction("Index");
             }
             else
             {
                 return View("Form", usuario);
             }
+        }
+
+        public ActionResult Index()
+        {
+            IList<Usuario> usuarios = usuarioDAO.Lista();
+            return View(usuarios);
         }
     }
 }
